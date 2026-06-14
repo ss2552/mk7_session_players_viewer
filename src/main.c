10c5a311a5064fdf62790a3d54f80da32d7e5e09
えす ESU <85072753+ss2552@users.noreply.github.com>
2026-06-14 12:39:50 +0000

    main.c の更新

--- src/main.c
+++ src/main.c
@@ -4,8 +4,8 @@
 #include "csvc.h"
 #include "plgldr.h"
 
-static Handle       thread;
-static Handle       memLayoutChanged;
+static Handle       g_ThreadHandle;
+static Handle       g_continueGameEvent;
 static u8           stack[STACK_SIZE] ALIGN(8);
 
 void init_libs(){
@@ -14,39 +14,32 @@ void init_libs(){
 
 static PluginMenu   menu;
 
-void main(){
-
-    init_libs();
-
-    u32 inputkey = 0;
-
-    svcControlProcess(CUR_PROCESS_HANDLE, PROCESSOP_GET_ON_MEMORY_CHANGE_EVENT, (u32)&memLayoutChanged, 0);
-    while(true){
+inline s32 Thread(){
         
         if(svcWaitSynchronization(memLayoutChanged, 10000000ULL) == 0x09401BFE){
-
             s32 event = PLGLDR__FetchEvent();
             switch(event){
                 case PLG_SLEEP_ENTRY:
                 case PLG_SLEEP_EXIT:
                 case PLG_ABOUT_TO_SWAP:
-                case PLG_ABOUT_TO_EXIT:
                     PLGLDR__Reply(event);
+                case PLG_ABOUT_TO_EXIT:
+                    break;
             }
+    }
+}
 
-            irrstScanInput();
-            inputkey = irrstKeyshold();
-            if(inputkey & KEY_ZL || inputkey & KEY_ZR){
-                PLGLDR__DisplayMenu(&menu);
-            }
+inline void main(){
 
-            continue;
-        }
+    u32 inputkey = 0;
+
+    irrstScanInput();
+    inputkey = irrstKeyshold();
+    if(inputkey & KEY_ZL || inputkey & KEY_ZR){
+        PLGLDR__DisplayMenu(&menu);
     }
 
     // irrstWaitForEvent(true);
-
-	   deinit_libs();
 
 }
 
@@ -54,22 +47,36 @@ void deinit_libs(){
     irrstExit();
 }
 
-int __entrypoint(int arg, void* temporaryStack){
-	
-	// PluginHeader *header = (PluginHeader *)0x07000000;
+void mainThread(){
 
-    // if(header->magic != HeaderMagic) return;
-    // __system_allocateHeaps(header);
+    // __sync_init();
+    // __system_initSyscalls();
+
+    init_libs();
+
+    svcSignalEvent(g_continueGameEvent);
+
+    memset(&menu, 0, sizeof(menu));
+
+    // svcControlProcess(CUR_PROCESS_HANDLE, PROCESSOP_GET_ON_MEMORY_CHANGE_EVENT, (u32)&memLayoutChanged, 0);
+
+    main();
+
+	   deinit_libs();
+
+    svcExitThread();
+
+}
+
+int __entrypoint(int arg, void* temporaryStack){
 	
     srvInit();
     plgLdrInit();
 
-    memset(&menu, 0, sizeof(menu));
-
-    svcCreateEvent(&thread, RESET_ONSHOT);
-    svcCreateThread(&thread, main, 0, (u32 *)(stack + STACK_SIZE), 30, -1);
-    svcWaitSynchronization(thread, U64_MAX);
-	svcCloseHandle(thread);
+    svcCreateEvent(&g_continueGameEvent, RESET_ONESHOT);
+    svcCreateThread(&g_ThreadHandle, mainThread, 0, (u32 *)(monitorStack + STACK_SIZE), 30, -1);
+    svcWaitSynchronization(g_continueGameEvent, U64_MAX);
+	svcCloseHandle(g_continueGameEvent);
 
     srvExit();
     plgLdrExit();
