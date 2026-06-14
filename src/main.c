8d9984a6111f8ba9375e1f2c531625c0ad84498e
えす ESU <85072753+ss2552@users.noreply.github.com>
2026-06-14 08:43:12 +0000

    c

--- src/main.c
+++ src/main.c
@@ -16,57 +16,42 @@ static PluginMenu   menu;
 
 void main(){
 
+    init_libs();
+
+    u32 inputkey = 0;
+
     svcControlProcess(CUR_PROCESS_HANDLE, PROCESSOP_GET_ON_MEMORY_CHANGE_EVENT, (u32)&memLayoutChanged, 0);
     while(true){
         
         if(svcWaitSynchronization(memLayoutChanged, 10000000ULL) == 0x09401BFE){
 
             s32 event = PLGLDR__FetchEvent();
+            switch(event){
+                case PLG_SLEEP_ENTRY:
+                case PLG_SLEEP_EXIT:
+                case PLG_ABOUT_TO_SWAP:
+                case PLG_ABOUT_TO_EXIT:
+                    PLGLDR__Reply(event);
+            }
 
-            if (event == PLG_SLEEP_ENTRY)
-            {
-                PLGLDR__Reply(event);
-            }
-            else if (event == PLG_SLEEP_EXIT)
-            {
-                PLGLDR__Reply(event);
-            }
-            else if (event == PLG_ABOUT_TO_SWAP)
-            {
-                PLGLDR__Reply(event);
-            }
-            else if (event == PLG_ABOUT_TO_EXIT)
-            {
-                PLGLDR__Reply(event);
-            }
-            if (HID_PAD & BUTTON_SELECT)
+            irrstScanInput();
+            inputkey = irrstKeyshold();
+            if(inputkey & KEY_ZL || inputkey & KEY_ZR){
                 PLGLDR__DisplayMenu(&menu);
-            ApplyCheat();
+            }
 
             continue;
         }
     }
 
-    memset(&menu, 0, sizeof(menu));
+    // irrstWaitForEvent(true);
 
-    init_libs();
+	   deinit_libs();
 
-    u32 inputkey = 0;
-
-    while(aptMainLoop()){
-        irrstWaitForEvent(true);
-        irrstScanInput();
-        inputkey = irrstKeyshold();
-        if(inputkey & KEY_ZL || inputkey & KEY_ZR){
-            PLGLDR__DisplayMenu(menu);
-        }
-    }
-
-	deinit_libs();
 }
 
 void deinit_libs(){
-	irrstExit();
+    irrstExit();
 }
 
 int __entrypoint(int arg, void* temporaryStack){
@@ -78,7 +63,9 @@ int __entrypoint(int arg, void* temporaryStack){
 	
     srvInit();
     plgLdrInit();
-    
+
+    memset(&menu, 0, sizeof(menu));
+
     svcCreateEvent(&thread, RESET_ONSHOT);
     svcCreateThread(&thread, main, 0, (u32 *)(stack + STACK_SIZE), 30, -1);
     svcWaitSynchronization(thread, U64_MAX);
