32cf21bd394d650d14fc66e096a30790dbd37ae2
えす ESU <85072753+ss2552@users.noreply.github.com>
2026-06-17 07:29:00 +0000

    utiils.c の更新

--- src/utiils.c
+++ src/utiils.c
@@ -1,12 +1,22 @@
 #include "utils.h"
 
-void     Flash(u32 color)
+void     Flash(bool isTop, u32 color)
 {
+    u32 *addr;
+
+    if(isTop){
+        addr = 0x10202204;
+    }else{
+        addr = 0x10202208;
+    }
+
     color |= 0x01000000;
     for (u32 i = 0; i < 64; i++)
     {
-        REG32(0x10202204) = color;
+        REG32(addr) = color;
         svcSleepThread(5000000);
     }
-    REG32(0x10202204) = 0;
+
+    REG32(addr) = 0;
+
 }
