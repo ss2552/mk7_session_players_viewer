500f4124c43962587bdf89076be8a9c8ca1699eb
えす ESU <85072753+ss2552@users.noreply.github.com>
2026-06-17 16:25:32 +0000

    main.c の更新

--- src/main.c
+++ src/main.c
@@ -25,9 +25,14 @@ void MonitorDeamon_Thread(){
     s32 event;
 
     while(true){
-        res = svcWaitSynchronization(memLayoutChanged, 10000000ULL);
 
+        Flash(false ,0x00, 0,0xFF);
+
+        res = svcWaitSynchronization(memLayoutChanged, 10000000ULL);
         if(res == 0x09401BFE){
+
+            LOCK = false;
+
             event = PLGLDR__FetchEvent();
             switch(event){
                 case PLG_SLEEP_ENTRY:
@@ -38,9 +43,6 @@ void MonitorDeamon_Thread(){
                 case PLG_ABOUT_TO_EXIT:
                     break;
             }
-
-            LOCK = false;
-
         }else{
 
             LOCK = true;
@@ -56,6 +58,8 @@ inline void main(){
     u32 inputkey = 0;
 
     while(aptMainLoop()){
+
+        Flash(false ,0x00, 0xFF, 0x0);
 
         svcSleepThread(10000000ULL);
 
@@ -91,7 +95,7 @@ void mainThread(){
 
     memset(&menu, 0, sizeof(menu));
 
-    Flash(true);
+    Flash(true, 0xFF, 0x00, 0xFF);
 
     main();
 
