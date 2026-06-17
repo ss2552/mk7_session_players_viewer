ba7931729905b5eea0fa46928d4dc810e30deaf9
えす ESU <85072753+ss2552@users.noreply.github.com>
2026-06-17 06:51:41 +0000

    main.c の更新

--- src/main.c
+++ src/main.c
@@ -3,33 +3,40 @@
 #include <stdio.h>
 #include "csvc.h"
 #include "plgldr.h"
+#include "menu.h"
 
 static Handle       g_ThreadHandle;
 static Handle       g_continueGameEvent;
 static u8           stack[STACK_SIZE] ALIGN(8);
 
+Result res;
+
 void init_libs(){
-	irrstInit();
+   	irrstInit();
 }
 
 static PluginMenu   menu;
 
 inline s32 Thread(){
-        
-        if(svcWaitSynchronization(memLayoutChanged, 10000000ULL) == 0x09401BFE){
-            s32 event = PLGLDR__FetchEvent();
-            switch(event){
-                case PLG_SLEEP_ENTRY:
-                case PLG_SLEEP_EXIT:
-                case PLG_ABOUT_TO_SWAP:
-                    PLGLDR__Reply(event);
-                case PLG_ABOUT_TO_EXIT:
+
+    res = svcWaitSynchronization(memLayoutChanged, 10000000ULL);
+
+    if(res == 0x09401BFE){
+        s32 event = PLGLDR__FetchEvent();
+        switch(event){
+            case PLG_SLEEP_ENTRY:
+            case PLG_SLEEP_EXIT:
+            case PLG_ABOUT_TO_SWAP:
+                PLGLDR__Reply(event);
+            case PLG_ABOUT_TO_EXIT:
                     break;
             }
     }
 }
 
 inline void main(){
+
+    init_menu();
 
     u32 inputkey = 0;
 
@@ -58,7 +65,7 @@ void mainThread(){
 
     memset(&menu, 0, sizeof(menu));
 
-    // svcControlProcess(CUR_PROCESS_HANDLE, PROCESSOP_GET_ON_MEMORY_CHANGE_EVENT, (u32)&memLayoutChanged, 0);
+    svcControlProcess(CUR_PROCESS_HANDLE, PROCESSOP_GET_ON_MEMORY_CHANGE_EVENT, (u32)&memLayoutChanged, 0);
 
     main();
 
@@ -74,7 +81,7 @@ int __entrypoint(int arg, void* temporaryStack){
     plgLdrInit();
 
     svcCreateEvent(&g_continueGameEvent, RESET_ONESHOT);
-    svcCreateThread(&g_ThreadHandle, mainThread, 0, (u32 *)(monitorStack + STACK_SIZE), 30, -1);
+    svcCreateThread(&g_ThreadHandle, mainThread, 0, (u32 *)(stack + STACK_SIZE), 30, -1);
     svcWaitSynchronization(g_continueGameEvent, U64_MAX);
 	svcCloseHandle(g_continueGameEvent);
 
