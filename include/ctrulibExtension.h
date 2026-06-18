fc0e7b4211e951b84116c27ab86fb99cc46e2d59
えす ESU <85072753+ss2552@users.noreply.github.com>
2026-06-18 09:40:03 +0000

    ctrulibExtension.h の作成

---
+++ include/ctrulibExtension.h
@@ -0,0 +1,8 @@
+extern const size_t __tdata_align;
+extern const u8 __tdata_lma[];
+extern const u8 __tdata_lma_end[];
+extern u8 __tls_start[];
+extern u8 __tls_end[];
+// Result fontEnsureMappedExtension(void);
+extern u32 __apt_appid;
+extern u32 __system_runflags;
