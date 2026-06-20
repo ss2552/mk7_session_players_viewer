903300f19760692d232eca68efb9bad262475dfa
えす ESU <85072753+ss2552@users.noreply.github.com>
2026-06-20 08:20:00 +0000

    utils.c の更新

--- src/utils.c
+++ src/utils.c
@@ -4,12 +4,25 @@
 #include "types.h"
 #include "plgldr.h"
 
+#define PA_PTR(addr) (void *)((u32)(addr) | 1 << 31)
+#define REG32(addr) (*(vu32 *)(PA_PTR(addr)))
+
 void Flash(bool isTop, u8 r, u8 g, u8 b){
 
-    volatile u32 *addr = isTop ? (volatile u32 *)0x10202204 : (volatile u32 *)0x10202208;
+    u8 p = isTop ? 0x4 : 0x8;
 
-    u32 color = (1U << 31) |  0x01000000U | ((u32)b << 16) | ((u32)g << 8) | (u32)r;
+    u32 color = ((u32)b << 16) | ((u32)g << 8) | (u32)r;
 
+    color |= 0x01000000;
+
+    for (u32 i = 0; i < 64; i++){
+        REG32(0x10202200 | p) = color;
+        svcSleepThread(5000000);
+    }
+
+    REG32(0x10202200 | p) = 0;
+
+/*
     // char title[32];
     // char body[32];
     // snprintf(title, sizeof(title), "%p", (void*)addr);
@@ -23,5 +36,7 @@ void Flash(bool isTop, u8 r, u8 g, u8 b){
     }
 
     *addr = 0;
+
+*/
 
 }
